fbd09ebb2974b7907df79e9c99f2e47cbe5731fd
应佳伟 <yingjiawei@junleizg.com>
2018-07-23 09:43:30 +0800

    merge local

---
+++ lib/ui/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      constraints: const BoxConstraints.expand(),
+      color: Colors.red,
+    );
+  }
+
+}
+
